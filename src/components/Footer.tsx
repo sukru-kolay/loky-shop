c08d5c634a07d607ab2abcec9adafd094290e57c
sukru kolay <sukruextraegitim@outlook.com>
2023-09-03 17:18:39 +0300

    form input

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,24 +3,35 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footerDiv">
-      <ul className="footerList">
-        <h3>Destek</h3>
-        <li>
-          <a href="#">Bize Ulaşın</a>
-        </li>
-        <li>
-          <a href="#">Kargo ve İade</a>
-        </li>
-        <li>
-          <a href="#">Beden Ölçüleri</a>
-        </li>
-        <li>
-          <a href="#">Gizşlilik Sözleşmesi</a>
-        </li>
-        <li>
-          <a href="#">Kullanıcı Sözleşmesi</a>
-        </li>
-      </ul>
+      <div className="footerLeft">
+        <ul className="footerList">
+          <h3>Destek</h3>
+          <li>
+            <a href="#">Bize Ulaşın</a>
+          </li>
+          <li>
+            <a href="#">Kargo ve İade</a>
+          </li>
+          <li>
+            <a href="#">Beden Ölçüleri</a>
+          </li>
+          <li>
+            <a href="#">Gizşlilik Sözleşmesi</a>
+          </li>
+          <li>
+            <a href="#">Kullanıcı Sözleşmesi</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footerRight">
+        <p className="footerFormTitle">
+          <span>LOKY'DEN</span>
+          <br />
+          <span> HABERLER</span>
+        </p>
+        <input className="footerInput" placeholder="EPOSTA" type="email" />
+        <button>Gönder</button>
+      </div>
     </div>
   );
 };
